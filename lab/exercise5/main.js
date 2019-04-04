99744c41a0bf8e0c5846da57b956dfa7e37a5b9c
sohyun lim <lims789@newschool.edu>
2019-04-04 14:54:04 -0400

    ww
    wewew

---
+++ lab/exercise5/main.js
@@ -0,0 +1,50 @@
+alert ("Welcome! Please view console on your browser.");
+
+var myName = "sohyun lim";
+console.log(myName);
+
+var myNumber = 20;
+console.log(myNumber);
+
+var myFood = "hotdog is my favorite";
+console.log(myFood);
+
+var call = true;
+console.log(call);
+
+var myMy = myNumber + myFood; 
+console.log (myMy);
+console.log(3 * 8);
+console.log(48 / 2);
+console.log(12 + 12);
+console.log(4 * 6);
+
+
+var countries = ["Croatia", "Argentina", "Russia"];
+console.log(countries[0]);
+console.log(countries.length);
+
+var currentTime = "The current time is";
+var time = new Date().getHours() + ":" + new Date().getMinutes();
+console.log(currentTime + time)
+
+var currentHour = new Date().getHours();
+if(currentHour == 11) 
+{console.log("it is 11 now")}
+else {console.log("it is not 11 yet.")};
+
+console.log (myName.length);
+myName = myName.length - 1;
+
+if (myName <= 9) 
+  {console.log("My name is relatively short.")
+} else if (myName >= 14) 
+  {console.log("My name is relatively long.")
+} else 
+  {console.log("My name is neither short nor long.")
+};
+
+
+
+
+
