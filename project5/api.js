7f7f8db5dcfa7df283496b1be4ac914cb1c93e61
sohyun lim <lims789@newschool.edu>
2019-04-19 13:56:19 -0400

    ss

--- project5/api.js
+++ project5/api.js
@@ -35,3 +35,58 @@ $(document).ready(function(){
 
 
 });
+//part2
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?id=3410315&units=metric&APPID=7425438545a87f72f8e3b5ce7175bc40', //CHANGE THE URL TO YOUR API QUERY
+        dataType: 'jsonp',
+        success: function(results){
+            weathertext2 = results.weather[0].main;
+            temp2 = results.main.temp;
+            humidity2 = results.main.humidity;
+        
+
+            myWeatherInterpretation();
+
+        }
+    });
+    
+
+    function myWeatherInterpretation(){
+        //THIS IS WHERE YOU CAN CUSTOMIZE YOUR PAGE'S FUNCTIONS    
+        
+       
+        $('p2').append('temp: ' + temp2);
+        $('p2').append('humidity: ' + humidity2);
+        $('p2').append('weathertext: ' + weathertext2);
+        
+        //END OF MYWEATHERINTERPRETATION FUNCTION
+    }   
+//part3
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?id=3186886&units=metric&APPID=7425438545a87f72f8e3b5ce7175bc40', //CHANGE THE URL TO YOUR API QUERY
+        dataType: 'jsonp',
+        success: function(results){
+            weathertext3 = results.weather[0].main;
+            temp3 = results.main.temp;
+            humidity3 = results.main.humidity;
+        
+
+            myWeatherInterpretation();
+
+        }
+    });
+    
+
+    function myWeatherInterpretation(){
+        //THIS IS WHERE YOU CAN CUSTOMIZE YOUR PAGE'S FUNCTIONS    
+        
+       
+        $('p3').append('temp: ' + temp3);
+        $('p3').append('humidity: ' + humidity3);
+        $('p3').append('weathertext: ' + weathertext3);
+        
+        //END OF MYWEATHERINTERPRETATION FUNCTION
+    }   
+
+
+
