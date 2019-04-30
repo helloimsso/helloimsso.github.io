c17e9b06bb67ca14389449a27aba4f771a3f9ac0
sohyun lim <lims789@newschool.edu>
2019-04-30 13:00:55 -0400

    ss

--- project5/api/api.js
+++ project5/api/api.js
@@ -79,7 +79,7 @@ $(function() {
 $(function() {
   console.log('weather');
 
-  var url = 'http://api.openweathermap.org/data/2.5/weather?id=3410315&units=metric&APPID=7425438545a87f72f8e3b5ce7175bc40'
+  var url = 'https://api.openweathermap.org/data/2.5/weather?id=3410315&units=metric&APPID=7425438545a87f72f8e3b5ce7175bc40'
    $.get(url, function(data) {
 
      console.log(data);
