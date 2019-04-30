969ad2359b5d580a952faad7b023a723c6d19a02
sohyun lim <lims789@newschool.edu>
2019-04-30 13:02:51 -0400

    ss

--- project5/api/api.js
+++ project5/api/api.js
@@ -4,6 +4,42 @@ $(function() {
 
 var url = 'https://api.openweathermap.org/data/2.5/weather?id=3410315&units=metric&APPID=7425438545a87f72f8e3b5ce7175bc40'
   $.get(url, function(data) {
+var temp; 
+var humidity; //Humidity, %
+var weathertext; //Group of weather parameters (Rain, Snow, Extreme etc.)
+
+
+$(document).ready(function(){
+
+
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?id=1835848&units=metric&APPID=7425438545a87f72f8e3b5ce7175bc40', //CHANGE THE URL TO YOUR API QUERY
+        dataType: 'jsonp',
+        success: function(results){
+            weathertext = results.weather[0].main;
+            temp = results.main.temp;
+            humidity = results.main.humidity;
+        
+
+            myWeatherInterpretation();
+
+        }
+    });
+    
+
+    function myWeatherInterpretation(){
+        //THIS IS WHERE YOU CAN CUSTOMIZE YOUR PAGE'S FUNCTIONS    
+        
+       
+        $('p').append('temp: ' + temp);
+        $('p').append('humidity: ' + humidity);
+        $('p').append('weathertext: ' + weathertext);
+        
+        //END OF MYWEATHERINTERPRETATION FUNCTION
+    }   
+
+
+});
 
 
     var temperature = data.main.temp;
